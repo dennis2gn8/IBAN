178c5d675a5411b7ae9ae239efa8ff270b6d5304
diogioz <dennis.andre@gmx.de>
2014-11-24 12:29:26 +0100

    my first commit

---
+++ IBAN/IBANnumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBAN
+{
+    public class IBANnumber
+    {
+        public bool Validate(string iban)
+        {
+
+            int length; 
+            length= iban.Length;
+
+            if (length == 0)          
+            {
+                return false;
+            }
+            else if (length < 35) 
+            {
+                return true;
+            }
+
+            return false;
+        
+        }
+    }
+}
+
---
+++ IBANTest/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IBAN;
+
+namespace IBANTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IBANnumber numberclass = new IBANnumber();
+            String Zieferanzahl;
+            Zieferanzahl = "111111111111111111111111111111111";
+            bool result;
+            result = numberclass.Validate(Zieferanzahl);
+            Assert.IsTrue(result);
+            
+        }
+
+        [TestMethod]
+        public void TestMethod2()     
+        {   
+            IBANnumber numberclass = new IBANnumber();
+            String Zieferanzahl1;
+            Zieferanzahl1 = "";
+            bool result;
+            result = numberclass.Validate(Zieferanzahl1);
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            IBANnumber numberclass = new IBANnumber();
+            String Zieferanzahl2;
+            Zieferanzahl2 = "66666666666666666666666666666666677777777777777777777777777777777777777777777777777777777777777777777";
+            bool result;
+            result = numberclass.Validate(Zieferanzahl2);
+            Assert.IsTrue(result);
+
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            IBANnumber numberclass = new IBANnumber();
+            String Zieferanzahl2;
+            Zieferanzahl2 = "1234567890123456789012345678901234";
+            bool result;
+            result = numberclass.Validate(Zieferanzahl2);
+            Assert.IsTrue(result);
+        }
+    }  
+}
+    
