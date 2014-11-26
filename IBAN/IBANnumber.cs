e1992d99875efcfa2779ec5291acfff08987f4d2
diogioz <dennis.andre@gmx.de>
2014-11-26 10:51:37 +0100

    new commit

--- IBAN/IBANnumber.cs
+++ IBAN/IBANnumber.cs
@@ -6,9 +6,17 @@ using System.Threading.Tasks;
 
 namespace IBAN
 {
+    /// <summary>
+    /// f
+    /// </summary>
     public class IBANnumber
     {
-        public bool Validate(string iban)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="iban"></param>
+        /// <returns></returns>
+        public bool isValid(string iban)
         {
 
             int length; 
@@ -24,8 +32,47 @@ namespace IBAN
             }
 
             return false;
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="letter"></param>
+        /// <returns></returns>
+        public bool isLetter(string letter)
+        {
+            if (letter.StartsWith("DE"))
+                return true;
+            else
+                return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numeral"></param>
+        /// <returns></returns>
+        public bool isNumeral(string numeral)
+        {
+            string erg = numeral.Substring(2, 2);
+            return erg.All(c => Char.IsNumber(c));
         
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool isNumber(string number)
+        {
+            string erg = number.Substring(4, 30);
+            return erg.All(c => Char.IsNumber(c));
+            
+        }
     }
+        
 }
+
 
