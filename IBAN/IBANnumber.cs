8def86f33a17321dfb8cedefd70ebb64eeee4e2e
diogioz <dennis.andre@gmx.de>
2015-01-21 17:26:17 +0100

    Version 6
    Logik wurde erweitert
         -BLZ P Prüffunktion  implementiert
          -Konto Prüffunktion implementiert

--- IBAN/IBANnumber.cs
+++ IBAN/IBANnumber.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace IBAN
 {
@@ -27,39 +28,30 @@ namespace IBAN
             {
                 return false;
             }
-               
-            //ich brauche ein paar string parameters, und evtl. einen parameter
-            string LeanderCode = iban.Substring(0, 2);
-            //Eine definierte Variable mit den parameter Pruefsumme , die eine Wert enthält
-            string Pruefsumme = iban.Substring(2, 2);
-            //Eine definierte Variable mit den parameter Pruefsumme , die eine Wert enthält
-            string BLZ_Konto = iban.Substring(4);
-            //Hier wird die Bankleitzahl und Kontonummer extrahiert.
 
+           // string KontrollZ = iban.Substring()
             // Ein Array mit der Größe 2
             var zahlencode = new[] { 1, 2 };
 
-            if (!Pruefsumme.All(c => Char.IsNumber(c)))
+            if (!iban.Substring(0, 2).All(c => Char.IsLetter(c)))
             {
                 return false;
             }
 
-            if (!BLZ_Konto.All(c => Char.IsNumber(c)))
+            if (!iban.Substring(2).All(c => Char.IsNumber(c)))
             {
                 return false;
             }
-
-            if (!LeanderCode.All(c => Char.IsLetter(c)))
+            
+            if (iban.StartsWith("DE") && iban.Length != 22)
             {
                 return false;
             }
+            else if (iban.StartsWith("PL")&& iban.Length != 28|| iban.StartsWith ("CZ") && iban.Length != 24)
 
-            if (LeanderCode.StartsWith("DE") && iban.Length != 22)
-            {
                 return false;
-            }           
 
-            char[] zeichen = LeanderCode.ToCharArray();
+            char[] zeichen = iban.Substring(0, 2).ToCharArray();
 
             //Gibt mir die Position der einzelnen Zeichen an (+1) 
             for (int i = 0; i < zeichen.Length; i++)
@@ -71,14 +63,14 @@ namespace IBAN
             // Ich erstelle eine Variable egdy in welcher ich aus einen Zahlenarray einen String verkette.
             string egdy = string.Concat(zahlencode);
             //Hier erste ich Variable mit den Namen resultString der mir die Buchstaben durch Zahlen resetzt.
-            string resultString = iban.Replace(LeanderCode, egdy);
+            string resultString = iban.Replace(iban.Substring(0, 2), egdy);
             //In diesen Codeabschnitt findet eine Umstellung statt.
             resultString = resultString.Substring(6) + resultString.Substring(0, 6);
 
             //In diesen Abschnitt wer eine Variable erstellt die den Parameter "zwischenwert" enthält 
-            decimal erg = Convert.ToDecimal(decimal.Parse(resultString) % 97);
+            BigInteger erg = BigInteger.Parse(resultString) % 97;
             
-            /// Hier wurde überprüft ob das Ergebnis 1 ergibt.
+            // Hier wurde überprüft ob das Ergebnis 1 ergibt.
             if (erg > 1 || erg < 1)
             {
                 return false;
@@ -90,14 +82,57 @@ namespace IBAN
 
         public string GetBLZ(string iban)
         {
-            Boolean bla = IsValid(iban);
-            String blz = string.Empty;
-            if (bla == true) 
+            Boolean Valid = IsValid(iban);
+
+            if (Valid == true)
             {
+                if (iban.StartsWith("DE"))
+                {
+                    return iban.Substring(4,8);
+                }
+                else if (iban.StartsWith("PL"))
+                {
+                    return iban.Substring(4,8);
+                }
+                else if (iban.StartsWith("CZ"))
+                    return iban.Substring(4,4);
+            
+            }
+            
                 
+                return string.Empty;
+
+
+        }
+
+
+        public string GetKonto(string iban)
+        {
+            Boolean Valid = IsValid(iban);
+
+            if (Valid == true)
+            {
+                if (iban.StartsWith("DE"))
+                {
+                    return iban.Substring(12);
+                }
+                else if (iban.StartsWith("PL"))
+                {
+                    return iban.Substring(12);
+                }
+                else if (iban.StartsWith("CZ"))
+                    return iban.Substring(10);
+            
+            }
+
+
+            
+
 
             }
-            return blz;
+
+
+
         }
            
     }
--- IBANTest/UnitTest1.cs
+++ IBANTest/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace IBANTest
         /// In diesem Beispiel wird die Zeichenlänge überprüft und ein "true" ausgegeben , wenn die Bedingung erfüllt ist.
         /// </summary>
         [TestMethod]
-        public void IsValid()
+        public void IsValidDE()
         {
             //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
             IBANnumber numberclass = new IBANnumber();
@@ -24,7 +24,7 @@ namespace IBANTest
         }
 
         [TestMethod]
-        public void IsValid2()
+        public void IsValidPL()
         {
             //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
             IBANnumber numberclass = new IBANnumber();
@@ -36,7 +36,7 @@ namespace IBANTest
         }
 
         [TestMethod]
-        public void IsValid3()
+        public void IsValidDK()
         {
             //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
             IBANnumber numberclass = new IBANnumber();
@@ -48,17 +48,146 @@ namespace IBANTest
         }
 
         [TestMethod]
-        public void TestGetBLZ()
+        public void TestGetDEBLZ()
         {
             //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
             IBANnumber numberclass = new IBANnumber();
             String iban;
-            iban = "DK3456789012345678";
+            iban = "DE10550604170000082716";
             String result;
             result = numberclass.GetBLZ(iban);
-            Assert.AreSame()
+            Assert.AreEqual(result, "55060417");
+       
         }
-        //forget programming, PLAY SMITE INSTEAD (Proginator)
+
+        [TestMethod]
+        public void TestGetDEBLZNOT()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "DE1055060417000023213213082716";
+            String result;
+            result = numberclass.GetBLZ(iban);
+            Assert.AreNotEqual(result, "55060417");
+       
+         }
+
+        [TestMethod]
+        public void TestGetDEKonto()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "DE10550604170000082716";
+            String result;
+            result = numberclass.Ausgabe(iban);
+            Assert.AreEqual(result, "0000082716");
+       
+        }
+
+        [TestMethod]
+        public void TestGetDEKontoNot()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "DE1055060417000023213213082716";
+            String result;
+            result = numberclass.Ausgabe(iban);
+            Assert.AreNotEqual(result, "0000082716");
+        }
+
+        [TestMethod]
+        public void TestGetPLBLZ()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "PL37109024020000000610000434";
+            String result;
+            result = numberclass.GetBLZ(iban);
+            Assert.AreEqual(result, "10902402");
+        }
+
+        [TestMethod]
+        public void TestGetNotPLBLZ()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "PL37109024020655000000610000434";
+            String result;
+            result = numberclass.GetBLZ(iban);
+            Assert.AreNotEqual(result, "10902402");
+            
+         }
+
+        [TestMethod]
+        public void TestGetPLKonto()
+        {
+                //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+                IBANnumber numberclass = new IBANnumber();
+                String iban;
+                iban = "PL37109024020000000610000434";
+                String result;
+                result = numberclass.Ausgabe(iban);
+                Assert.AreEqual(result,"0000000610000434");
+        }
+
+        [TestMethod]
+        public void TestGetCZBLZ()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "CZ6508000000192000145399";
+            String result;
+            result = numberclass.GetBLZ(iban);
+            Assert.AreEqual(result, "0800");
+        }
+
+        [TestMethod]
+        public void TestGetNoTCZBLZ()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "CZ65034253458000000192000145399";
+            String result;
+            result = numberclass.GetBLZ(iban);
+            Assert.AreNotEqual(result, "0800");
+        }
+        [TestMethod]
+        public void TestGetCZKonto()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "CZ6508000000192000145399";
+            String result;
+            result = numberclass.Ausgabe(iban);
+            Assert.AreEqual(result, "00192000145399");
+        }
+                 [TestMethod]
+        public void TestGetNotKonto()
+        {
+            //ich instanziiere hier eine Klasse und übergebe die  einer neuen  Klasse
+            IBANnumber numberclass = new IBANnumber();
+            String iban;
+            iban = "CZ650342534580sadasda00000192000145399";
+            String result;
+            result = numberclass.Ausgabe(iban);
+            Assert.AreNotEqual(result,"0000192000145399");
+
+
+
+
+
+
+
+        }
+        
     }
 }  
 
